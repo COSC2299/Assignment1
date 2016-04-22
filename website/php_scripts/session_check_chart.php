2329d8813544f83518ad8848b893351c6b503159
rmit-s3488184-janith-muthuhetti <muthuhetti@imac-27.internal.muthuhetti.com>
2016-04-22 18:56:08 +1000

    remove debugging code

--- website/php_scripts/session_check_chart.php
+++ website/php_scripts/session_check_chart.php
@@ -1,14 +1,11 @@
 <?php
 	if ($_SESSION['newSession'] != false && $_SESSION['ready'] == true){
-		echo $_SESSION['openChart'];
 		if ($_SESSION['openChart'] == false && isset($_COOKIE['chartURL'])){
-			echo '<script></script>';
 			echo '<script>';
 			echo "window.open('".$_COOKIE['chartURL']."', 'chartWindow', 'width=1300, height=1000')"; 
 			echo '</script>';
 			$_SESSION['openChart'] = true;
 		}
-		echo $_SESSION['openChart'];
 	}
 	else{
 		$_SESSION['ready'] = true;
